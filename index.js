24cca73f790155a7229c6ef7132693d413c149a4
Khaled Karim <kskarim177@gmail.com>
2022-05-08 21:43:08 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,6 +11,23 @@ const res = require('express/lib/response');
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('deco', decoded);
+        req.decoded = decoded;
+        next();
+    })
+    // console.log('verifyJWT', authHeader);
+
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mzzkb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -18,6 +36,15 @@ async function run() {
     try {
         await client.connect();
         const stockCollection = client.db(`virtualWarehouse`).collection('stock');
+
+        // auth
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
 
         //get all stocks
         app.get('/stock', async (req, res) => {
@@ -35,8 +62,30 @@ async function run() {
             res.send(stock)
         })
 
+        app.get('/singlestock', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            // console.log(email)
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = stockCollection.find(query);
+                const stocks = await cursor.toArray();
+                res.send(stocks)
+            }
+            else {
+                res.status(403).send({ message: 'forbidden access' })
+            }
+
+        })
+
         // delete stock from inventory
         app.delete('/stock/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await stockCollection.deleteOne(query);
+            res.send(result);
+        })
+        app.delete('/singlestock', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
             const result = await stockCollection.deleteOne(query);
@@ -65,6 +114,8 @@ async function run() {
             res.send(result);
 
         })
+
+
 
     }
     finally {
